7ffb2d7242db91dfa90d5d8679a61a708c4a719a
“abdullah017” <dev.abdullahtas@gmail.com>
2025-04-17 23:45:28 +0300

    fix

--- lib/flutter_share.dart
+++ lib/flutter_share.dart
@@ -4,11 +4,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/foundation.dart';
+import 'flutter_share_platform_interface.dart';
 
 /// A Flutter plugin for sharing content via the platform share UI.
 class FlutterShare {
-  static const MethodChannel _channel = MethodChannel('flutter_share');
-
   /// Shares a message or/and link url with app chooser.
   ///
   /// - Title: Is the [title] of the message. Used as email subject if sharing
@@ -17,25 +16,18 @@ class FlutterShare {
   /// - LinkUrl: Is the [linkUrl] to include with the message.
   /// - ChooserTitle (Just for Android): Is the [chooserTitle] of the app
   /// chooser popup. If null the system default title will be used.
-  static Future<bool?> share(
-      {required String title,
-      String? text,
-      String? linkUrl,
-      String? chooserTitle}) async {
-    assert(title.isNotEmpty);
-
-    if (title.isEmpty) {
-      throw FlutterError('Title cannot be null');
-    }
-
-    final success = await _channel.invokeMethod('share', <String, dynamic>{
-      'title': title,
-      'text': text,
-      'linkUrl': linkUrl,
-      'chooserTitle': chooserTitle,
-    });
-
-    return success;
+  static Future<bool?> share({
+    required String title,
+    String? text,
+    String? linkUrl,
+    String? chooserTitle,
+  }) {
+    return FlutterSharePlatform.instance.share(
+      title: title,
+      text: text,
+      linkUrl: linkUrl,
+      chooserTitle: chooserTitle,
+    );
   }
 
   /// Shares a local file with the app chooser.
@@ -48,30 +40,19 @@ class FlutterShare {
   /// chooser popup. If null, the system default title will be used.
   /// - FileType (Just for Android): It's the [fileType] that will be sent in the 
   /// chooser popup. If null, the system default title will be used.
-  static Future<bool?> shareFile(
-      {required String title,
-      required String filePath,
-      String? text,
-      String? chooserTitle,
-      String fileType = '*/*'}) async {
-    assert(title.isNotEmpty);
-    assert(filePath.isNotEmpty);
-
-    if (title.isEmpty) {
-      throw FlutterError('Title cannot be null');
-    } else if (filePath.isEmpty) {
-      throw FlutterError('FilePath cannot be null');
-    }
-
-    final success =
-        await _channel.invokeMethod('shareFile', <String, dynamic>{
-      'title': title,
-      'text': text,
-      'filePath': filePath,
-      'fileType': fileType,
-      'chooserTitle': chooserTitle,
-    });
-
-    return success;
+  static Future<bool?> shareFile({
+    required String title,
+    required String filePath,
+    String? text,
+    String? chooserTitle,
+    String fileType = '*/*',
+  }) {
+    return FlutterSharePlatform.instance.shareFile(
+      title: title,
+      filePath: filePath,
+      text: text,
+      chooserTitle: chooserTitle,
+      fileType: fileType,
+    );
   }
 }
